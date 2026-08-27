77a0f201b41bdd82e8bd3d2d76e92afc541547de
Angesh Karthik S <angeshangath020@gmail.com>
2026-08-27 22:12:55 +0530

    Add Two Sum using HashMap

---
+++ practice.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+int main(){
+    char str[100];
+    fgets(str,100,stdin);
+    str[strcspn(str,"\n")]=0;
+    char ans[strlen(str)+1];
+    int j=0;
+    for(int i=0;str[i]!='\0';i++){
+        char ch=tolower(str[i]);
+        if(isalnum(ch)){
+            ans[j++]=ch;
+        }
+    }
+    ans[j]='\0';
+    int flag=1;
+    int left=0;
+    int right=strlen(ans)-1;
+    while(left<right){
+        if(ans[left]!=ans[right]){
+            flag=0;
+            break;
+        }
+        left++;
+        right--;
+    }
+    if(flag)
+        printf("Valid Palindrome");
+    else
+        printf("Not a Valid Palindrome");
+
+    return 0;
+}
