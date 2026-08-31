a8c9852fcfa278fe36b1e85fd6b0ee830fa17ece
Angesh Karthik S <angeshangath020@gmail.com>
2026-08-31 19:35:53 +0530

    Refactor email and card number masking logic

--- Strings/Easy/Masking Credentials.c
+++ Strings/Easy/Masking Credentials.c
@@ -1,25 +1,59 @@
 #include<stdio.h>
 #include<string.h>
 #include<ctype.h>
-int main(){
-  char str[100];
-  fgets(str,100,stdin);
-  str[strcspn(str,"\n")]='\0';
-  for(int i=0;str[i]!='\0';i++){
-    if(isalpha(str[i])){
-      for(int x=1;str[x]!='@';x++){
-        str[x]='*';
-      }
-    }
-  }
-  for(int i=0;str[i]!='\0';i++){
-    if(isdigit(str[i])){
-      for(int x=0;x<14;x++){
-        if(isdigit(str[x])){
-          str[x]='*';
+
+int main()
+{
+    char str[100];
+
+    fgets(str,100,stdin);
+    str[strcspn(str,"\n")]='\0';
+
+    if(strchr(str,'@') != NULL)
+    {
+        // Email masking
+        for(int i=1; str[i]!='@'; i++)
+        {
+            if(isalpha(str[i]) || isdigit(str[i]) || str[i]=='.')
+            {
+                str[i]='*';
+            }
         }
-      }
     }
-  }
-  printf("%s",str);
+    else
+    {
+        // Card number masking
+        int len = strlen(str);
+        int digitcount = 0;
+
+        for(int i=0; i<len; i++)
+        {
+            if(isdigit(str[i]))
+            {
+                digitcount++;
+            }
+        }
+
+        int keep = 4;
+
+        for(int i=0; i<len; i++)
+        {
+            if(isdigit(str[i]))
+            {
+                if(digitcount > keep)
+                {
+                    str[i]='*';
+                    digitcount--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+
+    printf("%s",str);
+
+    return 0;
 }
