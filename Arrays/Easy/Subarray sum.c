75d4d3713318032f7014b1e22c2d3ca450276c38
Angesh Karthik S <angeshangath020@gmail.com>
2026-09-01 20:45:31 +0530

    Add input handling for array size and target sum

--- Arrays/Easy/Subarray sum.c
+++ Arrays/Easy/Subarray sum.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
 int main(){
+  
   int n;
   scanf("%d",&n);
   int arr[n];
+  
   for(int i=0;i<n;i++){
     scanf("%d",&arr[i]);
   }
+  
   int target;
   scanf("%d",&target);
+  
   for(int i=0;i<n;i++){
     int sum=0;
     for(int j=i;j<n;j++){
