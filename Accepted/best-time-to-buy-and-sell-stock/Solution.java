5bb2e7ef16c6159f05c02005d3b2da766f2d11e5
Angesh Karthik S <angeshangath020@gmail.com>
2026-09-21 19:16:22 +0530

    Sync Solution: best-time-to-buy-and-sell-stock

---
+++ Accepted/best-time-to-buy-and-sell-stock/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] arr) {
+        int min=arr[0];
+        int profit=0;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+            int max = arr[i]-min;
+            if(max>profit){
+                profit=max;
+            }
+        }
+        return profit;
+    }
+}
