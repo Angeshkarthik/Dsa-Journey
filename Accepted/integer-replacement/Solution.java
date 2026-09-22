06068ef4f3532e7ec73f18dfae7b7587f64035d4
Angesh Karthik S <angeshangath020@gmail.com>
2026-09-22 10:10:03 +0530

    Sync Solution: integer-replacement

---
+++ Accepted/integer-replacement/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int integerReplacement(int num) {
+        long n = num;
+        int count=0;
+        while(n!=1){
+            if(n%2==0){
+                n=n/2;
+            }
+            else if(n==3){
+                n=n-1;
+            }
+            else if((n&2)==0){
+                n=n-1;
+            }
+            else{
+                n=n+1;
+            }
+            count++;
+        }
+        return count;
+    }
+}
