42cccd8db0c51df0fa5ee953300a8a244901545a
Angesh Karthik S <angeshangath020@gmail.com>
2026-09-25 22:20:46 +0530

    Sync Solution: subarray-sum-equals-k

---
+++ Accepted/subarray-sum-equals-k/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int subarraySum(int[] arr, int k) {
+        int n = arr.length;
+        HashMap<Integer,Integer>map = new HashMap<>();
+        int sum=0;
+        int count=0;
+        map.put(0,1);
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            int req = sum-k;
+            if(map.containsKey(req)){
+                count+=map.get(req);
+            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        return count;
+    }
+}
